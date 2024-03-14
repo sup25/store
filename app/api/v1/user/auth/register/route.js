a409d64ddfe9f019b1aeaba1f2197217170f5e0a
Elnatanv <elnatanv@gmail.com>
2024-03-14 18:52:04 +0100

    fix: i created external db on cloud service so we can share the same data, and i fixed the fodler structore

--- app/api/v1/user/auth/register/route.js
+++ app/api/v1/user/auth/register/route.js
@@ -4,7 +4,19 @@ import { createUserValidation } from "../validation";
 export async function POST(request) {
   const body = await request.json();
 
-  createUserValidation(body);
+  try {
+    createUserValidation(body);
+    const user = await createUserController(body);
 
-  await createUserController(body);
+    return Response.json(
+      {
+        message: "User registered successfully",
+        data: { user },
+      },
+      { status: 201 }
+    );
+  } catch (err) {
+    console.log(err);
+    return Response.json({ error: "Internal Server Error" }, { status: 500 });
+  }
 }
--- app/api/v1/user/auth/untils/hashPassword/index.js
+++ app/api/v1/user/auth/untils/hashPassword/index.js
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+const bcrypt = require("bcrypt");
 
 export const hashPassword = async (password) => {
   try {
