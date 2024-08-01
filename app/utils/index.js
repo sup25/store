12af84b2d0159c115beed8775fc0d9b6bc986c4c
sup25 <suparnaa51@gmail.com>
2024-08-01 18:22:48 +0545

    fixing the utils

--- app/utils/index.js
+++ app/utils/index.js
@@ -2,29 +2,20 @@
 import appConfig from "@/config";
 import axios from "axios";
 
-const getBaseUrl = () => {
-  if (process.env.NODE_ENV === "production") {
-    return appConfig.baseUrl;
-  }
-  return "http://localhost:3000";
-};
-
 export const getAllProducts = async () => {
-  const response = await axios.get(`${getBaseUrl()}/api/v1/admin/auth/product`);
+  const response = await axios.get(`/api/v1/admin/auth/product`);
   return response.data.returnedData;
 };
 
 export const fetchProductsByTag = async (tag) => {
-  const response = await axios.get(`${getBaseUrl()}/api/v1/user/products`, {
+  const response = await axios.get(`/api/v1/user/products`, {
     params: { tags: tag },
   });
   return response.data.returnedData;
 };
 
 export const getPurchasedProducts = async ({ userId }) => {
-  const response = await axios.get(
-    `${getBaseUrl()}/api/v1/user/products/order/${userId}`
-  );
+  const response = await axios.get(`/api/v1/user/products/order/${userId}`);
   console.log("response: ", response);
   return response.data.returnedData;
 };
