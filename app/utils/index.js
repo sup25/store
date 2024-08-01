d22c27c5b955a5a151873b01af6055fefa056e8f
sup25 <suparnaa51@gmail.com>
2024-08-01 18:09:37 +0545

    checking the dynamic url in utils

--- app/utils/index.js
+++ app/utils/index.js
@@ -2,26 +2,28 @@
 import appConfig from "@/config";
 import axios from "axios";
 
+const getBaseUrl = () => {
+  if (process.env.NODE_ENV === "production") {
+    return appConfig.baseUrl;
+  }
+  return "http://localhost:3000";
+};
+
 export const getAllProducts = async () => {
-  const response = await axios.get(
-    `${appConfig.baseUrl}/api/v1/admin/auth/product`
-  );
+  const response = await axios.get(`${getBaseUrl()}/api/v1/admin/auth/product`);
   return response.data.returnedData;
 };
 
 export const fetchProductsByTag = async (tag) => {
-  const response = await axios.get(
-    `http://localhost:3000/api/v1/user/products`,
-    {
-      params: { tags: tag },
-    }
-  );
+  const response = await axios.get(`${getBaseUrl()}/api/v1/user/products`, {
+    params: { tags: tag },
+  });
   return response.data.returnedData;
 };
 
 export const getPurchasedProducts = async ({ userId }) => {
   const response = await axios.get(
-    `${appConfig.baseUrl}/api/v1/user/products/order/${userId}`
+    `${getBaseUrl()}/api/v1/user/products/order/${userId}`
   );
   console.log("response: ", response);
   return response.data.returnedData;
--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const appConfig = {
-  baseUrl: "http://localhost:3000" || process.env.NEXT_PUBLIC_BASE_URL,
+  baseUrl: "http://localhost:3000",
 };
 
 export default appConfig;
