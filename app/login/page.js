4dc16f51d66a1314815107352fe482e65b761bb7
sup25 <suparnaa51@gmail.com>
2024-08-01 17:05:23 +0545

    checking url

--- app/login/page.js
+++ app/login/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -17,6 +17,10 @@ const Login = ({ isPopup, redirectToVerification }) => {
     email: "",
     password: "",
   });
+
+  useEffect(() => {
+    console.log("Axios Base URL:", axiosClient.defaults.baseURL);
+  }, []);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
