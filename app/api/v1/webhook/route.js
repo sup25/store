7e441e309c4a96ce91d3c44cf8308abd4d9f36dc
sup25 <suparnaa51@gmail.com>
2024-08-03 09:00:55 +0545

    test

--- app/api/v1/webhook/route.js
+++ app/api/v1/webhook/route.js
@@ -4,7 +4,7 @@ import { headers } from "next/headers";
 import { createOrderController } from "../admin/auth/order/controller";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2024-04-10",
+  apiVersion: "2023-10-16",
 });
 
 export async function POST(request) {
