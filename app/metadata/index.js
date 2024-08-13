edcfcd5c4a01242d65e526b0164a9e017f63c65a
sup25 <suparnaa51@gmail.com>
2024-08-13 15:44:56 +0545

    updated metadata to show the logo

--- app/metadata/index.js
+++ app/metadata/index.js
@@ -4,8 +4,18 @@ export async function generateMetadata({ params }) {
   return {
     title: productId
       ? `Product ${productId} - Store`
-      : "Store | Shope Conveniently",
+      : "Store | Shop Conveniently",
     description:
       "Discover a wide range of stylish and affordable clothing at our marketplace. Shop the latest trends and timeless classics with secure payment options and fast delivery. Find your perfect outfit today!",
+    openGraph: {
+      images: [
+        {
+          url: "/logo-no-background.svg",
+          width: 1200,
+          height: 630,
+          alt: "Store CKMZ Logo",
+        },
+      ],
+    },
   };
 }
