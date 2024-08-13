9ea92bd49d71e266d65c5bd826c9887f43a9de35
sup25 <suparnaa51@gmail.com>
2024-08-13 15:53:08 +0545

    fix meta description

--- app/metadata/index.js
+++ app/metadata/index.js
@@ -6,16 +6,6 @@ export async function generateMetadata({ params }) {
       ? `Product ${productId} - Store`
       : "Store | Shop Conveniently",
     description:
-      "Discover a wide range of stylish and affordable clothing at our marketplace. Shop the latest trends and timeless classics with secure payment options and fast delivery. Find your perfect outfit today!",
-    openGraph: {
-      images: [
-        {
-          url: "/logo-no-background.svg",
-          width: 1200,
-          height: 630,
-          alt: "Store CKMZ Logo",
-        },
-      ],
-    },
+      "Shop stylish, affordable clothing at our marketplace. Enjoy secure payment options, fast delivery, and the latest trends. Find your perfect outfit today",
   };
 }
