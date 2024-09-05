af6c10184cb68d51a95e90e7127404aea38e7ec7
sup25 <suparnaa51@gmail.com>
2024-09-05 19:21:25 +0545

    test/webhook-2

--- app/api/v1/webhook/route.js
+++ app/api/v1/webhook/route.js
@@ -9,12 +9,6 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2023-10-16",
 });
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
 const createTransporter = () => {
   return nodemailer.createTransport({
     service: "Gmail",
